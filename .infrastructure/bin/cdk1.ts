0a91317dae88bd086fb521f5760f2ad6568d5b20
ccx1010sky <ccx1010@gmail.com>
2024-03-22 10:22:20 +0000

    env setup

--- .infrastructure/bin/cdk1.ts
+++ .infrastructure/bin/cdk1.ts
@@ -16,7 +16,7 @@ const stack = new Cdk1Stack(app, 'Cdk1', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: '471112499382', region: 'eu-west-2' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
