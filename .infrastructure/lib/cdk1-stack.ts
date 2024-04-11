2f64e49ebd88f4300729da6741a91f93c35f35ad
ccx1010sky <ccx1010@gmail.com>
2024-04-11 23:33:20 +0100

    zip lambda.ts

--- .infrastructure/lib/cdk1-stack.ts
+++ .infrastructure/lib/cdk1-stack.ts
@@ -182,7 +182,7 @@ export default class Cdk1Stack extends cdk.Stack {
 
       functionProps: {
         memorySize: 3008,
-        code: Code.fromAsset(path.join(__dirname, '../../api/src')),
+        code: Code.fromAsset(path.join(__dirname, '../../api/src/lambda.ts.zip')),
 
         // code: Code.fromAsset(path.join(__dirname, './src.zip')),
         // code: Code.fromBucket(builds, 'api.zip'), // This can be uncommented once you've run a build of the API code
