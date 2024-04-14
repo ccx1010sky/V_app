6df4bdb0b764f9191ab312dd60527129a91a353e
ccx1010sky <ccx1010@gmail.com>
2024-04-14 13:55:39 +0100

    test1

--- api/src/routes/ping.ts
+++ api/src/routes/ping.ts
@@ -18,7 +18,7 @@ export async function ping(request: Request): Promise<Response> {
   console.log('ping', request.query);
 
 
-  //test api.yml
+  //test1 api.yml
   // Retrieve item from DynamoDB
   try {
     const { itemId } = request.query; // Assuming itemId is present in the request query
